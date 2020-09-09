a8a007b7987b6b857cbad9c6639884cdf6b897d4
James Belta <james.belta@gmail.com>
2020-09-09 17:08:11 -0600

    "Building test story 4. Merchant views users links and merchant dashboard link"

---
+++ spec/features/merchants/merchant_navigation_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "As a merchant employee I see the same links as a regular user" do
+
+  before :each do
+    @employee = User.create(name: "Jim Bob",
+      address: "2020 Whiskey River Blvd",
+      city: "Bamaville",
+      state: "AL",
+      zip: "33675",
+      email: "jimbobwoowoo@aol.com",
+      password: "merica4lyfe",
+      role: 1)
+
+
+    end
+
+  it "I see the same links as a regular user and a link to my merchant dashboard" do
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@employee)
+
+    visit "/"
+    save_and_open_page
+
+    expect(page).to have_link("Home")
+    expect(page).to have_link("All Merchants")
+    expect(page).to have_link("Profile")
+    expect(page).to have_link("Logout")
+    expect(page).to have_link("Merchant Dashboard")
+
+  end
+end
