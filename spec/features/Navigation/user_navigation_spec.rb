7ac18f2d4772e3bbbb979ee4a47fc95f191c0ca4
Chris Allbritton <cla.inbox@gmail.com>
2020-09-09 08:48:30 -0400

    Refactor user_navigation_spec test to accurately test default role

--- spec/features/Navigation/user_navigation_spec.rb
+++ spec/features/Navigation/user_navigation_spec.rb
@@ -1,28 +1,27 @@
 require "rails_helper"
 
 RSpec.describe "As a default user" do
-  it "I see the same links as a visitor plus a link to login and logout" do
-    it "I do not see a link to log in or register and see a flash confirmation" do
-      user_1 = User.create(name: "Jim Bob",
-                           address: "2020 Whiskey River Blvd",
-                           city: "Bamaville",
-                           state: "AL",
-                           zip: "33675",
-                           email: "jimbobwoowoo@aol.com",
-                           password: "merica4lyfe",
-                           role: 0)
+  it "I see the same links as a visitor plus a link to login and logout
+  I do not see a link to log in or register and see a flash confirmation" do
+    user = User.create(name: "Jim Bob",
+                       address: "2020 Whiskey River Blvd",
+                       city: "Bamaville",
+                       state: "AL",
+                       zip: "33675",
+                       email: "jimbobwoowoo@aol.com",
+                       password: "merica4lyfe",
+                       role: 0)
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      visit "/home"
+    visit "/"
 
-      within '.topnav' do
-        expect(page).to have_link("Profile")
-        expect(page).to have_link("Logout")
+    within '.topnav' do
+      expect(page).to have_link("Profile")
+      expect(page).to have_link("Logout")
 
-        expect(page).to_not have_link("Login")
-        expect(page).to_not have_link("Register")
-      end
+      expect(page).to_not have_link("Login")
+      expect(page).to_not have_link("Register")
     end
   end
 end
