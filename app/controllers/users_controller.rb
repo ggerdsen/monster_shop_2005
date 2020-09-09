3bf57ebc1cbaaade9d573b4956ebf88c3f86a6e8
Drew Williams <drewwilliams5280@gmail.com>
2020-09-08 20:56:45 -0600

    Completes user story 11, ensures passwords must match

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class  UsersController < ApplicationController
-  
+
   def new
-    
+
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -15,15 +15,15 @@ class  UsersController < ApplicationController
       render :new
     end
   end
-  
+
   def show
-    
+
   end
-  
+
   private
-  
+
   def user_params
-    params.permit(:name, :address, :city, :state, :zip, :email, :password)
+    params.permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
   end
-  
+
 end
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -56,4 +56,58 @@ RSpec.describe "As a visitor" do
     expect(page).to have_content("Error: Please fill in all required information, ensuring your password and confirmation match")
 
   end
+
+  it "Returns me to form if registration form is missing details" do
+    visit '/merchants'
+
+    within '.topnav' do
+      click_on "Register"
+    end
+
+    expect(current_path).to eq("/register")
+
+    fill_in :name, with: "Garrett James Drew-Chris"
+    fill_in :address, with: "123 Main St."
+    fill_in :city, with: "Denver"
+    fill_in :state, with: "CO"
+    fill_in :zip, with: "80444"
+    fill_in :email, with: "us@turing.io"
+    fill_in :password, with: "passsword"
+    fill_in :password_confirmation, with: "password"
+
+    click_on "Submit"
+
+    expect(current_path).to eq("/register")
+
+    expect(User.count).to eq(0)
+    expect(page).to have_content("Error: Please fill in all required information, ensuring your password and confirmation match")
+
+  end
+
+  it "does not accept duplicate email addresses" do
+    visit '/merchants'
+
+    within '.topnav' do
+      click_on "Register"
+    end
+
+    expect(current_path).to eq("/register")
+
+    fill_in :name, with: "Garrett James Drew-Chris"
+    fill_in :address, with: "123 Main St."
+    fill_in :city, with: "Denver"
+    fill_in :state, with: "CO"
+    fill_in :zip, with: "80444"
+    fill_in :email, with: "us@turing.io"
+    fill_in :password, with: "passsword"
+    fill_in :password_confirmation, with: "password"
+
+    click_on "Submit"
+
+    expect(current_path).to eq("/register")
+
+    expect(User.count).to eq(0)
+    expect(page).to have_content("Error: Please fill in all required information, ensuring your password and confirmation match")
+
+  end
 end
