fadc0b444862d422e5032744c82bd8d1d453303e
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-09-10 10:39:13 -0600

    User story 13 progress

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,9 @@ Rails.application.routes.draw do
   post "/register", to: "users#create"
 
   get "/profile", to: "users#show"
+  
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
 
   get "/", to: "home#index"
 end
