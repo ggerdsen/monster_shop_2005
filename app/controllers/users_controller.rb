d3aa569ca877ad79f3bf45ff358ba6b72f6affd9
Chris Allbritton <cla.inbox@gmail.com>
2020-09-10 10:45:04 -0400

    Refactor Users controller to call user_id instead of :user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class  UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "You are now registered and logged in"
-      session[:user] = @user
+      session[:user_id] = @user.id
       redirect_to "/profile"
     else
       flash[:error] = @user.errors.full_messages.uniq.to_sentence
@@ -17,7 +17,8 @@ class  UsersController < ApplicationController
   end
 
   def show
-
+    @user = current_user
+    render file: "/public/404" unless current_user
   end
 
   private
