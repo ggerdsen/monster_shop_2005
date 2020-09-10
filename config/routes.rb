50de70996e9239919f76f4e9272ee3924b378f70
James Belta <james.belta@gmail.com>
2020-09-09 19:47:01 -0600

    Admin/ dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  namespace :admin do
+    get '/dashboard', to: 'dashboard#index'
+  end
+
   get "/merchants", to: "merchants#index"
   get "/merchants/new", to: "merchants#new"
   get "/merchants/:id", to: "merchants#show"
