2bbe7c3d4b7424e78d32f7f46688ff7fd609462c
James Belta <james.belta@gmail.com>
2020-09-09 21:56:03 -0600

    Build merchant employee nav restrictions

---
+++ spec/features/merchants/404_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe "as an employee" do
+  before :each do
+      @user = User.create(name: "Jim Bob",
+        address: "2020 Whiskey River Blvd",
+        city: "Bamaville",
+        state: "AL",
+        zip: "33675",
+        email: "jimbobwoowoo@aol.com",
+        password: "merica4lyfe",
+        role: 1)
+    end
+  it 'does not allow default user to see admin dashboard index' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      visit "/admin"
+save_and_open_page
+      #expect(page).to_not have_link("Home")
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+  end
