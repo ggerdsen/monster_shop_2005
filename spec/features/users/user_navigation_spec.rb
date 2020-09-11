793e1a17a80020d2624028748f6c0dc097f62e42
Chris Allbritton <cla.inbox@gmail.com>
2020-09-11 07:52:02 -0400

    Add user_navigation_spec to users directory during navigation directory refactor

---
+++ spec/features/users/user_navigation_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe "As a default user" do
+  it "I see the same links as a visitor plus a link to login and logout
+  I do not see a link to log in or register and see a flash confirmation" do
+    user = User.create(name: "Jim Bob",
+                       address: "2020 Whiskey River Blvd",
+                       city: "Bamaville",
+                       state: "AL",
+                       zip: "33675",
+                       email: "jimbobwoowoo@aol.com",
+                       password: "merica4lyfe",
+                       role: 0)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/"
+
+    within '.topnav' do
+      expect(page).to have_link("Profile")
+      expect(page).to have_link("Logout")
+
+      expect(page).to_not have_link("Login")
+      expect(page).to_not have_link("Register")
+    end
+  end
+end
