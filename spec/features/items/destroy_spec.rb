f9c087dd11b22898e6059ab2b12018594bb9d34a
Drew Williams <drewwilliams5280@gmail.com>
2020-09-12 00:16:41 -0600

    Fixes tests to work with new required table column information

--- spec/features/items/destroy_spec.rb
+++ spec/features/items/destroy_spec.rb
@@ -27,12 +27,13 @@ RSpec.describe 'item delete', type: :feature do
       expect(Review.where(id:review_1.id)).to be_empty
     end
 
-    it 'I can not delete items with orders' do 
+    it 'I can not delete items with orders' do
       bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      regular_user = User.create!(name: "Harry Richard", address: "1234 Bland St.", city: "Denver", state: "CO", zip: "80085", email: "regular_user@email.com", password: "123", role: 0)
       chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
       review_1 = chain.reviews.create(title: "Great place!", content: "They have great bike stuff and I'd recommend them to anyone.", rating: 5)
-      order_1 = Order.create!(name: 'Meg', address: '123 Stang St', city: 'Hershey', state: 'PA', zip: 80218)
-      order_1.item_orders.create!(item: chain, price: chain.price, quantity: 2)
+      order_1 = regular_user.orders.create!(name: 'Meg', address: '123 Stang St', city: 'Hershey', state: 'PA', zip: 80218)
+      order_1.item_orders.create!(item: chain, price: chain.price, quantity: 2, status: "pending")
 
       visit "/items/#{chain.id}"
 
