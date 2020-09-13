aaf8d66e5e1c2f16a75fd7a34376dd620b5cf083
Chris Allbritton <cla.inbox@gmail.com>
2020-09-13 10:44:22 -0400

    Add belongs to has many relationships to merchant and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User <ApplicationRecord
   has_secure_password
 
   has_many :orders
-  
+
+  belongs_to :merchant, optional: true
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
   validates_presence_of :name, :address, :city, :state, :zip
