bb12505ff8f690cedaa625433ab7476fa71a572a
Chris Allbritton <cla.inbox@gmail.com>
2020-09-16 09:56:51 -0400

    Add update_activation action to merchant items_controller

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -3,4 +3,16 @@ class Merchant::ItemsController < Merchant::BaseController
   def index
     @merchant = Merchant.find(current_user.merchant_id)
   end
-end 
+
+  def update_activation
+    item = Item.find(params[:id])
+    if item.active?
+      item.update(active?: false)
+      flash[:notice] = "#{item.name} has been deactivated."
+    else
+      item.update(active?: true)
+      flash[:notice] = "#{item.name} has been activated."
+    end
+      redirect_to "/merchant/items"
+  end
+end
