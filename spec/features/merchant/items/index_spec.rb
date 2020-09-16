e8157cb34d0ee9fc374fb1fc57930ceb14609519
Chris Allbritton <cla.inbox@gmail.com>
2020-09-16 08:11:05 -0400

    Update to give permissions to user1 to access merchant items index

--- spec/features/merchant/items/index_spec.rb
+++ spec/features/merchant/items/index_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe "As a merchant employee" do
       @zebra_tire = @meg.items.create!(name: "Zebraskins", description: "No zebras involved!", price: 200, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 22)
       @snake_tire = @meg.items.create!(name: "Snakeskins", description: "Snakes on a tire!", price: 200, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 22)
       @user1 = User.create!(name: "Jim Bob", address: "2020 Whiskey River Blvd", city: "Bamaville", state: "AL", zip: "33675", email: "jimbobwoowoo@aol.com", password: "merica4lyfe", role: 1, merchant_id: @meg.id)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
     end
 
     it "has a list of all of my items with their attributes" do
@@ -15,11 +17,11 @@ RSpec.describe "As a merchant employee" do
       visit "merchant/items"
 
       within ".items-#{@gator_tire.id}" do
-        expect(page).to have_content(@gator_tire.name)
-        expect(page).to have_content(@gator_tire.description)
+        expect(page).to have_content("Name: #{@gator_tire.name}")
+        expect(page).to have_content("Description: #{@gator_tire.description}")
         expect(page).to have_content("Price: $#{@gator_tire.price}")
         expect(page).to have_css("img[src*='#{@gator_tire.image}']")
-        expect(page).to have_content("Active")
+        expect(page).to have_content("Status: #{@gator_tire.status}")
         expect(page).to have_content("Inventory: #{@gator_tire.inventory}")
       end
     end
