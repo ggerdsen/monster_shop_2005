a058a3fb39b8df23dfef6c51a81b5598c8c19911
Drew Williams <drewwilliams5280@gmail.com>
2020-09-16 14:34:18 -0600

    Removes commented out text from test

--- spec/features/merchant/orders/show_spec.rb
+++ spec/features/merchant/orders/show_spec.rb
@@ -37,17 +37,4 @@ RSpec.describe "Order Packaged" do
       expect(page).to have_content(@tire.price)
       expect(page).to have_content(@item_order1.quantity)
     end
-
-#     User Story 49, Merchant sees an order show page
-#
-# As a merchant employee
-# When I visit an order show page from my dashboard
-# I see the recipients name and address that was used to create this order
-# I only see the items in the order that are being purchased from my merchant
-# I do not see any items in the order being purchased from other merchants
-# For each item, I see the following information:
-# - the name of the item, which is a link to my item's show page
-# - an image of the item
-# - my price for the item
-# - the quantity the user wants to purchase
 end
