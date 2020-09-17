3541025e6c5621568c93bf3028338d60a804ea96
Drew Williams <65911652+drewwilliams5280@users.noreply.github.com>
2020-09-16 19:35:47 -0600

    Revert "Made merchant/orders up to date on local"

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -4,19 +4,6 @@ class Merchant::ItemsController < Merchant::BaseController
     @merchant = Merchant.find(current_user.merchant_id)
   end
 
-  def new
-  end
-
-  def create
-    @merchant = Merchant.find(current_user.merchant_id)
-    item = @merchant.items.new(item_params)
-    if item.save
-      redirect_to "/merchant/items"
-    else
-      flash[:error] = item.errors.full_messages.to_sentence
-      render :new
-    end
-
   def update_activation
     item = Item.find(params[:id])
     if item.active?
