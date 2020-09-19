7ce096e94b7cef0900984a66b113d94f6000c7cc
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-09-19 14:19:26 -0600

    Create test for final story 3

---
+++ spec/features/merchant/bulk_discounts/delete_spec.rb
@@ -0,0 +1,91 @@
+require 'rails_helper'
+
+RSpec.describe "As a merchant employee" do
+  before :each do
+    @meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+    @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+
+    @employee_user = User.create!(name: "Mary Jane", address: "1234 Bland St.", city: "Denver", state: "CO", zip: "80085", email: "employee_user@email.com", password: "123", role: 1, merchant_id: @meg.id)
+    @regular_user = User.create!(name: "Harry Richard", address: "1234 Bland St.", city: "Denver", state: "CO", zip: "80085", email: "regular_user@email.com", password: "123", role: 0)
+
+    visit '/'
+    
+    within ".topnav" do
+      click_link("Login")
+    end
+    
+    fill_in :email, with: @employee_user.email
+    fill_in :password, with: @employee_user.password
+    click_on "Submit"
+    click_link("Merchant Dashboard")
+    expect(current_path).to eq("/merchant")
+    
+    click_link "Set My Discounts"
+    
+    expect(current_path).to eq("/merchant/bulk_discounts")
+    
+    click_link "Create New Discount"
+    
+    expect(BulkDiscount.last).to eq(nil)
+    
+    fill_in :title, with: "Labor Day Sale!"
+    fill_in :minimum_item_quantity, with: "5"
+    fill_in :percent_discount, with: "25"
+    
+    click_on "Create This Discount"
+    expect(BulkDiscount.last).to_not eq(nil)
+    expect(current_path).to eq("/merchant")
+
+  end
+  
+  it "I see a link to delete discounts on a merchant's bulk discount index" do
+    
+    visit "/merchants/#{@meg.id}/items"
+
+    expect(page).to have_content("Labor Day Sale! 25% off a group of like items when you purchase 5 or more!")
+    
+    visit "/merchant"
+    
+    click_link "Set My Discounts"
+    
+    expect(current_path).to eq("/merchant/bulk_discounts")
+    
+    expect(page).to have_content(BulkDiscount.last.title)
+    expect(BulkDiscount.last).to_not eq(nil)
+    expect(BulkDiscount.last.title).to eq("Labor Day Sale!")
+    expect(BulkDiscount.last.minimum_item_quantity).to eq(5)
+    expect(BulkDiscount.last.percent_discount).to eq(25.0)
+    
+    click_link "Delete Discount"
+    
+    expect(current_path).to eq("/merchant/bulk_discounts")
+
+    expect(BulkDiscount.last).to eq(nil)
+  
+    within ".topnav" do
+      click_link("Logout")
+    end
+
+    within ".topnav" do
+      click_link("Login")
+    end
+    
+    fill_in :email, with: @regular_user.email
+    fill_in :password, with: @regular_user.password
+    click_on "Submit"
+
+    visit "/merchants/#{@meg.id}/items"
+    expect(page).to_not have_content("Edited Title! 1% off a group of like items when you purchase 1")
+  
+  end
+end
+
+
+# Final story 3
+# As a merchant
+# When I visit my dashboard (/merchant/dashboard), I see a link to manage discounts
+# When I click this link, is see list of my current discounts
+# Next to each, I see a link to delete that discount
+# After clicking, my discount is destroyed
+# After deleting, I no long see this discounts' terms on the (/merchant/:id/items)
