d53b052c225b0b2b3aa4437faf26476edc172f00
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-09-19 14:02:10 -0600

    Make edit/update actions

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -1,5 +1,10 @@
 class Merchant::BulkDiscountsController < Merchant::BaseController
 
+  def index
+    @merchant = Merchant.find(current_user.merchant_id)
+    @discounts = @merchant.bulk_discounts
+  end
+
   def new
     @merchant = Merchant.find(current_user.merchant_id)
   end
@@ -12,6 +17,23 @@ class Merchant::BulkDiscountsController < Merchant::BaseController
     else
       flash[:error] = @merchant.errors.full_messages.to_sentence
       render :new
+    end
+  end
+  
+  def edit
+    @discount = BulkDiscount.find(params[:discount_id])
+  end
+  
+  def update
+    discount = BulkDiscount.find(params[:discount_id])
+    updated = discount.updated_at
+    discount.update(discount_params)
+    if discount.updated_at != updated
+      flash[:success] = "Discount Updated: #{discount_params[:title]} #{discount_params[:percent_discount]}% off of a group of like items when you purchase #{discount_params[:minimum_item_quantity]}!"
+      redirect_to "/merchant"
+    else
+      flash[:error] = @item.errors.full_messages.to_sentence
+      render :edit
     end
   end
   
